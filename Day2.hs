ebda1851146aae8b390751fb572482a99163c920
vsh <88970656+u32int@users.noreply.github.com>
2022-12-03 10:19:31 +0100

    use cons operator

--- Day2.hs
+++ Day2.hs
@@ -13,27 +13,25 @@ calcPts (left, right) = outcome + right + 1
 
 solve :: [String] -> Int
 solve [] = 0
-solve lines = ret
+solve (curr:rest) = ret
   where
-    curr_line = head lines
-    left  = curr_line !! 0 
-    right = curr_line !! 2 
-    ret = calcPts(ord(left) - ord('A'), ord(right) - ord('X')) + solve(tail lines)
+    left  = curr !! 0
+    right = curr !! 2
+    ret = calcPts(ord(left) - ord('A'), ord(right) - ord('X')) + solve rest
 
 
 solveTwo :: [String] -> Int
 solveTwo [] = 0
-solveTwo lines = ret
+solveTwo (curr:rest) = ret
   where
-    curr_line = head lines
-    right    = curr_line !! 2
-    ordleft  = ord(curr_line !! 0) - ord('A')
+    right    = curr !! 2
+    ordleft  = ord(curr !! 0) - ord('A')
     pts = case right of
       'X' -> 0 + (ordleft + 2) `mod` 3 + 1
       'Y' -> 3 + ordleft + 1
       'Z' -> 6 + (ordleft + 1) `mod` 3 + 1
 
-    ret = pts + (solveTwo $ tail lines)
+    ret = pts + (solveTwo $ rest)
 
 main :: IO ()
 main = do
